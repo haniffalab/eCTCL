cb33daf40270305b884e4b2b979440e85cbc3a85
Ciro Sanger farm22 <cramsuig@gmail.com>
2025-08-26 15:25:39 +0100

    update shell logger

--- code/logger.sh
+++ code/logger.sh
@@ -6,14 +6,15 @@ function logger() {
         printf "[padding_character, def:'#'] [log_type, def:INFO]\n"
         return 1
     fi
-    if [[ -z "${4}" ]]; then LOGTYPE="\033[1;32mINFO\033[0m"; else LOGTYPE="${4}"; fi
+    while [[ "${1}" =~ ^- ]] || [[ "${1}" =~ ^INFO ]]; do shift; done
+    [ -z "${4}" ] && LTYPE="\033[1;32mINFO\033[0m" || LTYPE="${4}"
     PNAME="$(git rev-parse --show-toplevel 2>&1)"
-    [ ! -z "(echo ${PNAME} | grep -q fatal)" ] && PNAME="$(basename $(pwd)) "
-    LOGLINE="[$(date '+%Y-%m-%d %T')] ${PNAME}${LOGTYPE} "
+    [ ! -z "(echo ${PNAME} | grep -q fatal)" ] && PNAME="$(basename $(pwd))"
+    LOGLINE="[$(date '+%Y-%m-%d %T')] ${LTYPE} ${PNAME} "
     LOGLINE="${LOGLINE}[${0##*/}:${FUNCNAME}:${BASH_LINENO}]"
     TEXT="${LOGLINE} :: ${1}" # echo -e "${LOGLINE} :: $*"
     echo -en "${TEXT} " # can stop here if you don't want to add padding
-    TOTAL_LENGTH=${2:-80}
+    TOTAL_LENGTH="${2:-80}"
     if [[ -z "${3}" ]]; then PAD_CHAR="#" ; else PAD_CHAR="${3}" ; fi
     TEXT_ALONE=$(echo ${1} | sed -E 's/.*[0-9]{2}m//g; s/.033\[0m//g')
     PAD_LENGTH=$((TOTAL_LENGTH - ${#TEXT_ALONE} - 1))
@@ -22,13 +23,15 @@ function logger() {
     fi
     echo
 }
-
-function logger_warn() {
-    logger "${1}" "${2:-60}" "${3:-#}" "\033[1;33mWARN\033[0m"
+function logger_info () {
+    logger "${1}" "${2:-80}" "${3:-#}" "\033[1;32mINFO\033[0m"
 }
-function logger_error() {
-    logger "${1}" "${2:-60}" "${3:-#}" "\033[1;31mERROR\033[0m"
+function logger_warn () {
+    logger "${1}" "${2:-80}" "${3:-#}" "\033[1;33mWARN\033[0m"
 }
-function logger_debug() {
-    logger "${1}" "${2:-60}" "${3:-#}" "\033[1;34mDEBUG\033[0m"
+function logger_error () {
+    logger "${1}" "${2:-80}" "${3:-#}" "\033[1;31mERROR\033[0m"
+}
+function logger_debug () {
+    logger "${1}" "${2:-80}" "${3:-#}" "\033[1;34mDEBUG\033[0m"
 }
